ee76d5f1d8d5f921cbd9f3097bbde0270e156783
Mona Lisa <gugacns94@gmail.com>
2019-07-02 19:02:18 +0200

    zadatak 1 i 2

---
+++ src/components/AppCars.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+      <ul>
+        <li v-for="(car,index) in cars" :key="index">
+          {{ `${car.brand} ${car.model} ${car.year} ${car.maxSpeed} ${car.isAutomatic} ${car.engine} ${car.numberOfDoors} ${car.id}` }}
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import { carsService } from '@/services/CarsService'
+
+export default {
+  data () {
+    return {
+      cars: [],
+      car: {
+        "brand": "",
+        "model": "",
+        "year": "",
+        "maxSpeed": "",
+        "isAutomatic": Boolean,
+        "engine": "",
+        "numberOfDoors": "",
+        "id": ""
+      }
+    }
+  },
+
+  created () {
+    carsService.getAll()
+      .then(response => {
+        this.cars = response.data
+      }).catch(e => {
+        console.log(e)
+      })
+  }
+}
+</script>
+
+<style>
+li {
+  list-style: none;
+}
+</style>
